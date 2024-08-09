551936cb7332c28d9b3ebcef45b9b33d31859879
Mikhail Mefodyev <hg-4ybak@mail.ru>
2024-08-09 21:16:22 +0300

    feat: add extension to paths

--- internal/exporters/exporter_factory.go
+++ internal/exporters/exporter_factory.go
@@ -7,16 +7,24 @@ import (
 	"github.com/hikkiyomi/passman/internal/exporters/mappers"
 )
 
+func addExtensionToPath(path, ext string) string {
+	if strings.HasSuffix(path, ext) {
+		return path
+	}
+
+	return path + "." + ext
+}
+
 func GetExporter(exporter, path, browser string) Exporter {
 	var result Exporter
 
 	switch {
 	case exporter == "csv" || strings.HasSuffix(path, ".csv"):
-		result = NewCsvExporter(path, mappers.GetMapper(browser))
+		result = NewCsvExporter(addExtensionToPath(path, "csv"), mappers.GetMapper(browser))
 	case exporter == "tsv" || strings.HasSuffix(path, ".tsv"):
-		result = NewTsvExporter(path, mappers.GetMapper(browser))
+		result = NewTsvExporter(addExtensionToPath(path, "tsv"), mappers.GetMapper(browser))
 	case exporter == "json" || strings.HasSuffix(path, ".json"):
-		result = NewJsonExporter(path)
+		result = NewJsonExporter(addExtensionToPath(path, "json"))
 	default:
 		log.Fatal("Could not get exporter by given type.")
 	}
